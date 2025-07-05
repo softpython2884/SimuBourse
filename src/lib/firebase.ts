740db51edb9b41ddca5b2c96aa3772cb5c12995f
Enzo prados <enzo.prados@gmail.com>
2025-07-05 18:39:28 +0000

    toujour pas....
    // Import the functions you need from the SDKs you need

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBsl_3iwowf_cZKoRm6SNXXk3Kn7-aAzCM",
+  authDomain: "simubourse.firebaseapp.com",
+  projectId: "simubourse",
+  storageBucket: "simubourse.appspot.com",
+  messagingSenderId: "943658338198",
+  appId: "1:943658338198:web:8d7b126e4ddec0e1557fb7"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
