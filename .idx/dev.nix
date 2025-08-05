4556f083366c2a372f813d6d1c9816a12e1a7c9b
Enzo prados <enzo.prados@gmail.com>
2025-08-05 11:14:43 +0000

    ./node_modules/@genkit-ai/core/lib/tracing.js ./node_modules/genkit/lib/

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -34,7 +34,7 @@
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = ["npm" "run" "prod" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
           manager = "web";
         };
       };
