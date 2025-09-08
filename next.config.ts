88fa436334556a64acf4716b13e122696eef22d0
Enzo prados <enzo.prados@gmail.com>
2025-09-08 17:21:15 +0000

    ok, bas att entre temps j'ai fait la db... mais pourtant :
    C:\Users\s

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['@genkit-ai/googleai'],
-    },
+    serverExternalPackages: ['@genkit-ai/googleai'],
 };
 
 export default nextConfig;
