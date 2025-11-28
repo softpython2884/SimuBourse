ca359cd4c8eb74070ca9d403fe0bf1ae716a968f
Enzo prados <enzo.prados@gmail.com>
2025-11-28 11:37:59 +0000

    ah aussi la db faut qu'elle se créer automatiquement au démarage du truc

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,15 +1,6 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+import Database from 'better-sqlite3';
 import * as schema from './schema';
-import * as d from 'dotenv';
-d.config({ path: '.env' });
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("La variable d'environnement DATABASE_URL est manquante.");
-}
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-export const db = drizzle(pool, { schema });
+const sqlite = new Database('sqlite.db');
+export const db = drizzle(sqlite, { schema });
